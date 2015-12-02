9bf4b4f3702cb71a39d93b87a94089c6d6c698e2
Stephan Köninger <stephan.koeninger@gbtec.de>
2015-12-02 16:54:45 +0100

    chore(background): add delay, when background text has reached EOL

--- directives/codeConsole.js
+++ directives/codeConsole.js
@@ -1,28 +1,54 @@
 angular.module('de.codearcs.website')
-    .directive('codeConsole', ['$http', '$interval', 'RandomFileService', function ($http, $interval, RandomFileService) {
+    .directive('codeConsole', ['$http', '$interval', '$timeout', '$q', 'RandomFileService', function ($http, $interval, $timeout, $q, RandomFileService) {
         return {
             template: '<div class="overlay"></div><pre class="javascript"></pre>',
             link: function (scope, element) {
+                var partialText = "";
+                var textPointer = 0;
+                var intervalSpeed = 15;
+                var codeBlock = element.find('pre');
+            
                 RandomFileService.getRandomFile()
                     .then(function (file) {
-                        var codeBlock = element.find('pre');
-                        var wholeText = file.data.split('');
-                        var partialText = "";
-                        var textPointer = 0;
-
-                        var interval = $interval(function() {
-                            if(textPointer <= wholeText.length) {
-                                partialText += wholeText[textPointer];
-                                codeBlock.text(partialText);
-                                textPointer++;
-                                hljs.highlightBlock(codeBlock[0]);
-                            } else {
-                                $interval.cancel(interval);
-                            }
-                        }, 15);
-
+                        var wholeTextLines = file.data.split('\n');
+                                                           
+                        processLines(wholeTextLines, 0);
+                        
+                        
                         hljs.highlightBlock(codeBlock[0]);
                     });
+                    
+                function processLines(lines, lineIndex) {
+                    return processLine(lines, lineIndex)
+                        .then(function() {
+                            if(lineIndex <= lines.length) {
+                                $timeout(function() {
+                                    return processLines(lines, ++lineIndex);
+                                }, 500);
+                            } 
+                        })
+                }
+                    
+                function processLine(lines, lineIdx) {
+                    var deferred = $q.defer();
+                    var idx = 0;
+                    var chars = lines[lineIdx].split('');
+                    var intervalSpeed = chars.join('').trim().length > 0 ? 20 : 0;
+                    var interval = $interval(function() {
+                        if(idx < chars.length) {
+                            partialText += chars[idx];
+                            codeBlock.text(partialText);
+                            idx++;
+                            
+                            hljs.highlightBlock(codeBlock[0]);
+                        } else {
+                            $interval.cancel(interval);
+                            deferred.resolve();
+                        }
+                    }, intervalSpeed);
+                    
+                    return deferred.promise;
+                }
             }
         }
     }]);
